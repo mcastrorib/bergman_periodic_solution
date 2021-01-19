b6e3d7d16ebfe3dbe62841038a80cc96b41267e1
Matheus Ribeiro <matheusdcribeiro@gmail.com>
2021-01-19 14:33:11 -0300

    mapping gk vector

--- main.py
+++ main.py
@@ -65,6 +65,55 @@ def map_g_to_dg(idx1, idx2, N):
     final_idx = new_i1 - new_i2
     final_idx += 2*N
     return final_idx
+
+def find_gk(kvec, g, points):
+    half = points//2
+    gk_index = IDX2C_3D(half, half, half, points)
+    gkvec = g[half, half, half]
+    diffvec = kvec - gkvec
+    distance = np.sqrt(np.dot(diffvec, diffvec))
+
+    kxmin = half
+    kxmax = points
+    kxinc = 1
+    if(kvec[0] < 0.0):
+        kxmax = -1
+        kxinc = -1
+    
+    kymin = half
+    kymax = points
+    kyinc = 1
+    if(kvec[1] < 0.0):
+        kymax = -1
+        kyinc = -1
+    
+    kzmin = half
+    kzmax = points
+    kzinc = 1
+    if(kvec[2] < 0.0):
+        kzmax = -1
+        kzinc = -1
+    
+    print("k = ", kvec)
+    print(gk_index, gkvec, distance)
+    print(kxmin, kxmax, kxinc)
+    print(kymin, kymax, kyinc)
+    print(kzmin, kzmax, kzinc)
+    for k in range(kzmin, kzmax, kzinc):
+        for j in range(kymin, kymax, kyinc):
+            for i in range(kxmin, kxmax, kxinc):
+                
+                diffvec = kvec - g[j, i, k]
+                new_distance = np.sqrt(np.dot(diffvec, diffvec))
+                print("i,j,k = ", i,j,k, "G = ", g[j, i, k], new_distance)
+
+                if(new_distance < distance):
+                    distance = new_distance
+                    gkvec = g[j, i, k]
+                    gk_index = IDX2C_3D(i, j, k, points)
+                    print(gk_index, gkvec, distance)             
+    
+    return gkvec, gk_index
     
 
 
@@ -76,7 +125,14 @@ R = 5.0
 N = int(1)
 w = 0.9999
 u = 1.0
-q_array = np.array([[0.01, 0.0, 0.0]])
+k_direction = np.array([1,0,0])
+ka_max = 0.1
+k_points = 5
+k_array = np.zeros([k_points, 3])
+k_linspace = np.linspace(ka_max/a, 0.0, k_points)
+for i in range(3):
+    k_array[:, i] = k_direction[i] * k_linspace
+ 
 
 volume = a**3
 porosity = 1.0 + (np.pi / 4.0) - (3.0 * np.pi * (R/a)**2) + ((8.0 / 3.0 ) * np.pi) * (R/a)**3 
@@ -84,7 +140,10 @@ points = 2*N + 1
 Nrange = np.arange(-N, N+1)
 
 rfreq = 0.5*a / float(N)
-[x, y, z] = np.meshgrid(rfreq * Nrange, rfreq * Nrange, rfreq * Nrange) 
+# [x, y, z] = np.meshgrid(rfreq * Nrange, rfreq * Nrange, rfreq * Nrange) 
+Rrange = np.linspace(-0.5*a, 0.5*a, 2*points + 1)[1:2*points:2]
+[x, y, z] = np.meshgrid(Rrange, Rrange, Rrange)
+
 
 gfreq = 2*np.pi/a
 [gx, gy, gz] = np.meshgrid(gfreq*Nrange, gfreq*Nrange, gfreq*Nrange)
@@ -92,16 +151,16 @@ gfreq = 2*np.pi/a
 pore_r = np.array(np.zeros([points, points, points]))
 matrix_r = np.array(np.zeros([points, points, points]))
 for k in range(points):
-    for j in range(points):
-        for i in range(points):
+    for i in range(points):
+        for j in range(points):
             px = x[i,j,k]
             py = y[i,j,k]
             pz = z[i,j,k]
             pore_r[i,j,k] = pore_function(px, py, pz, R)
 
 for k in range(points):
-    for j in range(points):
-        for i in range(points):
+    for i in range(points):
+        for j in range(points):
             px = x[i,j,k]
             py = y[i,j,k]
             pz = z[i,j,k]
@@ -109,16 +168,16 @@ for k in range(points):
 
 r = np.array(np.zeros([points, points, points, 3]))
 for k in range(points):
-    for j in range(points):
-        for i in range(points):
+    for i in range(points):
+        for j in range(points):
             r[i, j, k, 0] = x[i, j, k] 
             r[i, j, k, 1] = y[i, j, k]
             r[i, j, k, 2] = z[i, j, k] 
 
 g = np.array(np.zeros([points, points, points, 3]))
 for k in range(points):
-    for j in range(points):
-        for i in range(points):
+    for i in range(points):
+        for j in range(points):
             g[i, j, k, 0] = gx[i, j, k] 
             g[i, j, k, 1] = gy[i, j, k]
             g[i, j, k, 2] = gz[i, j, k] 
@@ -126,26 +185,28 @@ for k in range(points):
 # g - g' space
 dpoints = 4*N + 1            
 dNrange = np.arange(-2*N, 2*N+1)
+dRrange = np.linspace(-0.5*a, 0.5*a, 2*dpoints + 1)[1:2*dpoints:2]
 drfreq = 0.5*rfreq
 norm_factor = 1.0
 while (norm_factor < dpoints):
     norm_factor *= 2.0
 
 [dg_x, dg_y, dg_z] = np.meshgrid(gfreq * dNrange, gfreq * dNrange, gfreq * dNrange)
-[dx, dy, dz] = np.meshgrid(drfreq * dNrange, drfreq * dNrange, drfreq * dNrange) 
+[dx, dy, dz] = np.meshgrid(dRrange, dRrange, dRrange)
+# [dx, dy, dz] = np.meshgrid(drfreq * dNrange, drfreq * dNrange, drfreq * dNrange) 
 
 dr = np.array(np.zeros([dpoints, dpoints, dpoints, 3]))
 for k in range(dpoints):
-    for j in range(dpoints):
-        for i in range(dpoints):
+    for i in range(dpoints):
+        for j in range(dpoints):
             dr[i, j, k, 0] = dx[i, j, k] 
             dr[i, j, k, 1] = dy[i, j, k]
             dr[i, j, k, 2] = dz[i, j, k] 
 
 dg = np.array(np.zeros([dpoints, dpoints, dpoints, 3]))
 for k in range(dpoints):
-    for j in range(dpoints):
-        for i in range(dpoints):
+    for i in range(dpoints):
+        for j in range(dpoints):
             dg[i, j, k, 0] = dg_x[i, j, k] 
             dg[i, j, k, 1] = dg_y[i, j, k]
             dg[i, j, k, 2] = dg_z[i, j, k] 
@@ -155,16 +216,16 @@ for k in range(dpoints):
 pore_dr = np.array(np.zeros([dpoints, dpoints, dpoints]))
 matrix_dr = np.array(np.zeros([dpoints, dpoints, dpoints]))
 for k in range(dpoints):
-    for j in range(dpoints):
-        for i in range(dpoints):
+    for i in range(dpoints):
+        for j in range(dpoints):
             px = dx[i,j,k]
             py = dy[i,j,k]
             pz = dz[i,j,k]
             pore_dr[i,j,k] = pore_function(px, py, pz, R)
 
 for k in range(dpoints):
-    for j in range(dpoints):
-        for i in range(dpoints):
+    for i in range(dpoints):
+        for j in range(dpoints):
             px = dx[i,j,k]
             py = dy[i,j,k]
             pz = dz[i,j,k]
@@ -188,8 +249,8 @@ Tdg = np.asmatrix(np.zeros([rows, cols], dtype=complex))
 
 # W matrix
 for k in range(points):
-    for j in range(points):
-        for i in range(points):
+    for i in range(points):
+        for j in range(points):
             row_index = IDX2C_3D(i, j, k, points)
             
             for kk in range(points):
@@ -210,13 +271,13 @@ Rinv = np.linalg.inv(R)
 
 # U matrix
 for k in range(points):
-    for j in range(points):
-        for i in range(points):
+    for i in range(points):
+        for j in range(points):
             row_index = IDX2C_3D(i, j, k, points)
             
             for kk in range(points):
-                for jj in range(points):
-                    for ii in range(points):
+                for ii in range(points):
+                    for jj in range(points):
                         col_index = IDX2C_3D(ii, jj, kk, points)
                         di = map_g_to_dg(i,ii,N)
                         dj = map_g_to_dg(j,jj,N)
@@ -227,27 +288,32 @@ for row in range(rows):
     Udg[row, row] += 1.0
                         
 # Solve for q
-for q in q_array:
-    
-    # T matrix
-    for k in range(points):
-        for j in range(points):
-            for i in range(points):
-                row_index = IDX2C_3D(i, j, k, points)
-                
-                for kk in range(points):
-                    for jj in range(points):
-                        for ii in range(points):
-                            col_index = IDX2C_3D(ii, jj, kk, points)
-                            Tdg[row_index, col_index] = np.dot((q + g[i,j,k]), (q + g[ii,jj,kk])) * Udg[row_index, col_index] 
+# for q in q_array:
+kvec = k_array[0]
 
-    # V matrix
-    Vdg = Rinv.H * Tdg * Rinv
-    vals, vecs = np.linalg.eig(Vdg)
-    vec = vecs.transpose()
-    # Sort eigen values and its vector
-    inds = np.argsort(vals)
-    vals = vals[inds]
-    vecs = vecs[inds] 
-    weights = (1.0/w) * (R.H - ((1-w) * Rinv)) * (vecs)
+# Find gk in reciprocal lattice
+gkvec, gkvec_index = find_gk(kvec, g, points)
+qvec = kvec - gkvec
+
+# T matrix
+for k in range(points):
+    for i in range(points):
+        for j in range(points):
+            row_index = IDX2C_3D(i, j, k, points)
+            
+            for kk in range(points):
+                for ii in range(points):
+                    for jj in range(points):
+                        col_index = IDX2C_3D(ii, jj, kk, points)
+                        Tdg[row_index, col_index] = np.dot((qvec + g[i,j,k]), (qvec + g[ii,jj,kk])) * Udg[row_index, col_index] 
+
+# V matrix
+Vdg = Rinv.H * Tdg * Rinv
+vals, vecs = np.linalg.eig(Vdg)
+vec = vecs.transpose()
+# Sort eigen values and its vector
+inds = np.argsort(vals)
+vals = vals[inds]
+vecs = vecs[inds] 
+weights = (1.0/w) * (R.H - ((1-w) * Rinv)) * (vecs)
     
